5f893f450207684f074fed5180bdd9e2d18b8e62
MMendes24 <mmbusiness1@protonmail.com>
2020-07-15 14:59:25 -0400

    Axios, imports, and data working. Ready to code.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+const nasaURL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2019-5-23&api_key=Qk9nJggwnMMmooGAW4JlbP5qotpE67j6TS05AmOW'
+  useEffect(() => {
+    axios.get(nasaURL)
+    .then((nasaURL) => {
+      console.log("We can hear you loud and clear, New Orleans!")
+      console.log(nasaURL.data)
+    })
+    .catch(() => {
+      console.log("Sorry New Orleans its all dark!")
+    })
+  })
   return (
     <div className="App">
       <p>
